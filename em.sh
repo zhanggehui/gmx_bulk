c5ab2267cbe41423868f39dfce7642aeeb73078c
张戈辉 <zhanggehui@zhanggehuideMacBook-Pro.local>
2020-08-06 12:59:27 +0800

    update

--- em.sh
+++ em.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-for ((i=0;i<1;i++))
-do
-gmx grompp -f ./scripts/em.mdp  -c bulk.gro -p GO2.top \
--o ./$rundir/em.tpr -po ./$rundir/em-out -n waterlayer.ndx
-
+gmx grompp -f ./scripts/em.mdp  -c bulk_ion.gro -p bulk_ion.top -o ./$rundir/em.tpr -po ./$rundir/em-out
 cd $rundir
 $gmxrun -v -deffnm em
-#echo "potential" | gmx energy -f em.edr -o em-potential.xvg
-
 cp -rf em.gro ../GO2-afterem.gro
-cp -rf ../GO2-afterem.gro ../GO2-ion.gro
-
 cd ..
-done
 
--- nvt-equ.sh
+++ nvt-equ.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-gmx grompp -f ./$scriptsdir/nvt-equ.mdp -c GO2-afterem.gro -p GO2.top \
--o ./$rundir/nvt-equ.tpr -po ./$rundir/nvt-equ-out -n waterlayer.ndx
+gmx grompp -f ./$scriptsdir/nvt-equ.mdp -c GO2-afterem.gro -p bulk_ion.top \
+-o ./$rundir/nvt-equ.tpr -po ./$rundir/nvt-equ-out
 
 cd $rundir
 $gmxrun -v -deffnm nvt-equ
-#echo "temperature" | gmx energy -f nvt-equ.edr -o nvt-equ-tem.xvg
 cd ..
