f9724aeab9f9eb13ca0e3ee63d2acf85914c4cb7
zhanggehui <395510548@qq.com>
2021-04-09 12:30:09 +0800

    update

--- nvt-equ.sh
+++ nvt-equ.sh
@@ -1,2 +1,2 @@
-gmx grompp -f $scriptsdir/nvt-equ.mdp -c ../em/em.gro -p ../bulk_ion_pp.top -o ./nvt-equ.tpr
+gmx grompp -f $scriptsdir/nvt-equ.mdp -c ../em/em.gro -p ../bulk_ion_pp.top -o ./nvt-equ.tpr -maxwarn 1
 $gmxrun -v -deffnm nvt-equ
