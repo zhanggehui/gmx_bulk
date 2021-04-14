a549cc73278e24b0de98d0117cb235ca81de26b9
zhanggehui <395510548@qq.com>
2021-04-14 21:56:53 +0800

    update

--- analysis/residence_time.sh
+++ analysis/residence_time.sh
@@ -5,6 +5,7 @@ cd ./select
 echo -e "\"1st shell\" resname SOL and name OW and within 0.32 of group NA\n" | \
 gmx select -s ../nvt-equ.tpr -f ../nvt-equ.trr -n ../index.ndx -os -oc -oi -on -om -of -olt -b 5000 -e 50000
 cd ../
+rm -rf index.ndx
 mv ./select ../$rundir
 cd ../
 mv ./$rundir ../tmp_data/
