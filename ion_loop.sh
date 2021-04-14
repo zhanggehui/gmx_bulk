74b8981261c66f0e8245917b0ed3f24e5d007555
zhanggehui <395510548@qq.com>
2021-04-14 11:05:50 +0800

    update

--- ion_loop.sh
+++ ion_loop.sh
@@ -4,7 +4,15 @@ n_ions=${#ions[@]}
 for ((i=0;i<$n_ions;i++)); do
     ion=${ions[$i]}
     cd $ion
-    source /home/liufeng_pkuhpc/lustre2/zgh/sub_job/auto_run.sh \
-    gmx auto 4 /home/liufeng_pkuhpc/lustre3/zgh/gmx/gmx_bulk/md_scripts npt-equ.sh nptequ
+    if [ $1 == 'npt']; then
+        source /home/liufeng_pkuhpc/lustre2/zgh/sub_job/auto_run.sh \
+        gmx auto 4 /home/liufeng_pkuhpc/lustre3/zgh/gmx/gmx_bulk/md_scripts npt-equ.sh nptequ
+    elif [ $1 == 'nvt' ]; then
+        source /home/liufeng_pkuhpc/lustre2/zgh/sub_job/auto_run.sh \
+        gmx auto 4 /home/liufeng_pkuhpc/lustre3/zgh/gmx/gmx_bulk/md_scripts nvt-equ.sh nvtequ
+    elif [ $1 == 'rdf' ]; then
+        source /home/liufeng_pkuhpc/lustre2/zgh/sub_job/auto_run.sh \
+        gmx auto 1 /home/liufeng_pkuhpc/lustre3/zgh/gmx/gmx_bulk/md_scripts/analysis rdf_ion.sh rdf_bulk
+    fi
     cd ../
 done
