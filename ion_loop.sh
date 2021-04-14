157fa4e64936005c1a71673c4d70ff9cd1b9b686
zhanggehui <395510548@qq.com>
2021-04-14 14:31:55 +0800

    update

--- ion_loop.sh
+++ ion_loop.sh
@@ -4,7 +4,7 @@ n_ions=${#ions[@]}
 for ((i=0;i<$n_ions;i++)); do
     ion=${ions[$i]}
     cd $ion
-    if [ $1 == 'npt']; then
+    if [ $1 == 'npt' ]; then
         source /home/liufeng_pkuhpc/lustre2/zgh/sub_job/auto_run.sh \
         gmx auto 4 /home/liufeng_pkuhpc/lustre3/zgh/gmx/gmx_bulk/md_scripts npt-equ.sh nptequ
     elif [ $1 == 'nvt' ]; then
