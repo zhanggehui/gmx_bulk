a0a2a49c26f9d1b5e413c8fe52f12c0e28278e95
zhanggehui <395510548@qq.com>
2021-04-14 10:56:52 +0800

    update

--- ion_loop.sh
+++ ion_loop.sh
@@ -4,6 +4,7 @@ n_ions=${#ions[@]}
 for ((i=0;i<$n_ions;i++)); do
     ion=${ions[$i]}
     cd $ion
+    source /home/liufeng_pkuhpc/lustre2/zgh/sub_job/auto_run.sh \
     gmx auto 4 /home/liufeng_pkuhpc/lustre3/zgh/gmx/gmx_bulk/md_scripts npt-equ.sh nptequ
     cd ../
 done
