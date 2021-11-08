0c2c3fbd69927337e21b4fa1b370fbe0beb14902
zhanggehui <395510548@qq.com>
2021-11-08 23:50:33 +0800

    update

--- npt-equ.sh
+++ npt-equ.sh
@@ -1,2 +1,2 @@
-$gmx grompp -f $scriptsdir/npt-equ.mdp -c ../em/em.gro -p ../bulk_ion_pp.top -o ./npt-equ.tpr -maxwarn 1
+$gmx grompp -f $scriptsdir/npt-equ.mdp -c ../em/em.gro -p ../bulk_ion_pp.top -o ./npt-equ.tpr # -maxwarn 1
 $gmxrun -v -deffnm npt-equ
