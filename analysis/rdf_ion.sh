b417eddb23a0e7bf152bd85c84910189c9c94ab6
zhanggehui <395510548@qq.com>
2021-11-09 18:48:55 +0800

    update

--- analysis/rdf_ion.sh
+++ analysis/rdf_ion.sh
@@ -1,11 +1,12 @@
 source $scriptsdir/rdf_func.sh
 
 cd ../nvtequ
-ion=$rundir
+
+ion=${rundir%%_*}
 xvgfile=${ion}.xvg
 get_rdf $ion $xvgfile 50000
 mv $xvgfile ../$rundir
 mv cn.xvg ../$rundir
-cd ../
 
+cd ../
 mv ./$rundir ../tmp_data/$rundir
