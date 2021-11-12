f91cfc9e92d746b558df42ae012d8b8cfeec0756
zhanggehui <395510548@qq.com>
2021-11-12 20:48:50 +0800

    update

---
+++ equ.sh
@@ -0,0 +1,9 @@
+$gmx grompp -f $scriptsdir/npt-equ.mdp -c ../em/em.gro -p ../bulk_ion_pp.top -o ./npt-equ.tpr # -maxwarn 1
+$gmxrun -v -deffnm npt-equ
+
+cd ../
+mkdir nvtequ
+cd ./nvtequ
+
+$gmx grompp -f $scriptsdir/nvt-equ.mdp -c ../nptequ/npt-equ.gro -p ../bulk_ion_pp.top -o ./nvt-equ.tpr
+$gmxrun -v -deffnm nvt-equ
