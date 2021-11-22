eb0a7762f7d91bd35eccd318007a39f343c449d9
zhanggehui <395510548@qq.com>
2021-11-22 10:42:42 +0800

    update

--- analysis/residence_time.sh
+++ analysis/residence_time.sh
@@ -6,7 +6,7 @@ echo -e "a OW\nq" | gmx make_ndx -f nvt-equ.gro
 mkdir ./select
 cd ./select
 
-declare -A first_shell=(["LI"]="0.274" ["NA"]="0.322" ["K"]="0.354" ["CS"]="0.394")
+declare -A first_shell=(["LI"]="0.272" ["NA"]="0.314" ["K"]="0.35" ["CS"]="0.39")
 echo -e "\"1st shell\" resname SOL and name OW and within ${first_shell[$ion]} of group $ion\n" | \
 $gmx select -s ../nvt-equ.tpr -f ../nvt-equ.trr -n ../index.ndx -os -oc -oi -on -om -of -olt -b 5000 -e 50000
 
