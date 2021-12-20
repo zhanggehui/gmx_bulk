939d2d4fcb733553795857fd518ae875efd80810
zhanggehui <395510548@qq.com>
2021-12-20 17:08:45 +0800

    update

--- analysis/rdf.sh
+++ analysis/rdf.sh
@@ -1,6 +1,6 @@
 function get_rdf() {
     echo -e "a OW\nq" | gmx make_ndx -f nvt-equ.gro
-    gmx rdf -f nvt-equ.trr -n index.ndx -norm rdf -ref $1 -sel OW -selrpos atom -seltype atom -rmax 1 -o $2 -cn cn.xvg -b 5000 -e $3
+    gmx rdf -f nvt-equ.trr -n index.ndx -norm number_density -ref $1 -sel OW -selrpos atom -seltype atom -rmax 1 -o $2 -cn cn.xvg -b 5000 -e $3
     rm -rf \#* index.ndx
 }
 
